4b30c302ef4158d444396b60a9a2c7416cdd95d4
utsab <shakhawatutsab@gmail.com>
2024-06-24 06:41:14 +0600

    practice php variable & scope

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
 
+// PHP Variables
+$name = "Shakhawat Hossain Utsab";
+$age = 23;
+$profession = "Developer";
 
-$a = 10;
-$b = 20;
+echo "My name is $name and my age is $age and my profession is $profession" ;
 
-echo $a*$b;
+echo "</br>";
+echo "</br>";
+
+// Variable Scope
+
+$x =  100;
+$y = 200;
+
+function myTest(){
+	global $x,$y;
+
+	$y = $x + $y;
+}
+
+
+
+myTest();
+echo $y;
